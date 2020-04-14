2433b98e35b4d8ab19365dc09f690c17316f8e3e
Philippe Caron <philippecaron@rosaire.lan>
2020-04-13 20:18:18 -0400

    CRA with gameboard io

---
+++ jeu/src/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Client } from "boardgame.io/react";
+import { TicTacToe } from "./game";
+import { TicTacToeBoard } from "./board";
+import { Local } from "boardgame.io/multiplayer";
+import { SocketIO } from "boardgame.io/multiplayer";
+
+const TicTacToeClient = Client({
+  game: TicTacToe,
+  board: TicTacToeBoard,
+  multiplayer: SocketIO({ server: "localhost:8000" }),
+});
+
+class App extends React.Component {
+  state = { playerID: null };
+
+  render() {
+    if (this.state.playerID === null) {
+      return (
+        <div>
+          <p>Play as</p>
+          <button onClick={() => this.setState({ playerID: "0" })}>
+            Player 0
+          </button>
+          <button onClick={() => this.setState({ playerID: "1" })}>
+            Player 1
+          </button>
+        </div>
+      );
+    }
+    return (
+      <div>
+        <TicTacToeClient playerID={this.state.playerID} />
+      </div>
+    );
+  }
+}
+
+export default App;
