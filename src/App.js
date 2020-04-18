781d21d2d0d6680dca80da6c70d3d4420f1bd03b
Philippe Caron <philippecaron@rosaire.lan>
2020-04-17 20:12:06 -0400

    lobby integration, more robust server host

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import { Client } from "boardgame.io/react";
 import { Huit } from "./games/huit/game";
 import { HuitBoard } from "./games/huit/board";
 import { SocketIO } from "boardgame.io/multiplayer";
+import { Lobby } from "boardgame.io/react";
 
 const HuitClient = Client({
   game: Huit,
   board: HuitBoard,
-  multiplayer: SocketIO({ server: "http://localhost:8000" }),
+  multiplayer: SocketIO({
+    server: `${window.location.protocol}//${window.location.hostname}:8000`,
+  }),
 });
 
 class App extends React.Component {
@@ -28,22 +31,15 @@ class App extends React.Component {
   }
 
   render() {
-    return this.state.in ? (
-      <HuitClient playerID={this.state.playerID} />
-    ) : (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Inventez un pseudonyme
-            <input
-              type="text"
-              value={this.state.value}
-              onChange={this.handleChange}
-            />
-          </label>
-          <input type="submit" value="Jouer" />
-        </form>
-      </div>
+    return (
+      <>
+        <Lobby
+          gameServer={`${window.location.protocol}//${window.location.hostname}:8000`}
+          lobbyServer={`${window.location.protocol}//${window.location.hostname}:8000`}
+          gameComponents={[{ game: Huit, board: HuitBoard }]}
+        />
+        <HuitClient playerID={this.state.playerID} />
+      </>
     );
   }
 }
