671991810a1ac42b04f15c041704a5029f319f98
Philippe Caron <philippecaron@rosaire.lan>
2020-04-20 23:48:19 -0400

    removed typescript files

--- src/helpers/AddressHelper.ts
+++
@@ -1,6 +0,0 @@
-export default class AddressHelper {
-  /** Gets bgio host:port address. */
-  public static getServerAddress() {
-    return `${window.location.protocol}//${window.location.hostname}:8000`;
-  }
-}
--- src/helpers/SSRHelper.ts
+++
@@ -1,6 +0,0 @@
-// Returns true if SSR is rendering the page, else false.
-export default class SSRHelper {
-  public static isSSR() {
-    return typeof window === 'undefined';
-  }
-}
